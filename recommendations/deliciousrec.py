aefda710a079f54dad23a31d90883b782a5d6206
Bobby Lei <bobl53@gmail.com>
2016-09-19 15:06:27 +0800

    typo

--- recommendations/deliciousrec.py
+++ recommendations/deliciousrec.py
@@ -1,9 +1,9 @@
-from pydelicious import get_popular, get_userposts, get_userlposts
+from pydelicious import get_popular, get_userposts, get_urlposts
 
 def initializeUserDict(tag, count = 5):
   user_dict = {}
   for p1 in get_popular(tag = tag)[0:count]:
-    for p2 in get_urlpost(p1['href']):
+    for p2 in get_urlposts(p1['href']):
       user=p2['user']
       user_dict[user] = {}
   return user_dict
