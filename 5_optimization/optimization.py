82621ac05c972ad73cf928893a30a5e9f6f90b92
Bobby Lei <bobl53@gmail.comm>
2017-09-16 12:24:22 +0800

    added annealing optization

--- 5_optimization/optimization.py
+++ 5_optimization/optimization.py
@@ -93,6 +93,9 @@ def randomoptimize(domain, costf):
             bestr = r
     return bestr
 
+# s = optimization.hillclimb(domain, optimization.schedulecost)
+# Performs a little beter than the random algorithm but can get itself into
+# a local mininum
 def hillclimb(domain, costf):
     # Create a random solution
     sol = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
@@ -121,3 +124,39 @@ def hillclimb(domain, costf):
         if best == current: break
 
     return sol
+
+# s = optimization.annealingoptimize(domain, optimization.schedulecost)
+# Reduces the chances of getting stuck in a local minimum and increases
+# the chances of finding the global minimum. However, results may
+# not always be the most optimal as it does not always find this
+# global minimum
+def annealingoptimize(domain, costf, T = 10000.0, cool = 0.95, step = 1):
+    # Initialize the values randomly
+    vec = [int(random.randint(domain[i][0], domain[i][1]))
+            for i in range(len(domain))]
+
+    while T > 0.1:
+        # Choose one of the indices
+        i = random.randint(0, len(domain)-1)
+
+        # Choose a direction to change it
+        dir = random.randint(-step, step)
+
+        # Create a new list with one the values changed
+        vecb = vec[:]
+        vecb[i] += dir
+        if vecb[i] < domain[i][0]: vecb[i] = domain[i][0]
+        elif vecb[i] > domain[i][1]: vecb[i] = domain[i][1]
+
+        # Calculate the current cost and the new cost
+        ea = costf(vec)
+        eb = costf(vecb)
+        p = pow(math.e,(-eb-ea)/T)
+
+        # Is is better, or does it make the probability cutoff?
+        if (eb < ea or random.random() < p):
+            vec = vecb
+
+        # Decrease the temperature
+        T = T * cool
+    return vec
