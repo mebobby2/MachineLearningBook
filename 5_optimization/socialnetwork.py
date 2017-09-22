2c279c85ab09451e4af5d2abda1471459af796d6
Bobby Lei <bobl53@gmail.comm>
2017-09-22 19:27:22 +0800

    social network cost

--- 5_optimization/socialnetwork.py
+++ 5_optimization/socialnetwork.py
@@ -1,5 +1,13 @@
 import math
 
+# import socialnetwork
+# import optimization
+# sol=optimization.randomoptimize(socialnetwork.domain,socialnetwork.crosscount)
+# socialnetwork.crosscount(sol)
+# sol=optimization.annealingoptimize(socialnetwork.domain, socialnetwork.crosscount,step=50,cool=0.99)
+# socialnetwork.crosscount(sol)
+# sol
+
 people = ['Charlie', 'Augustus', 'Veruca', 'Violet', 'Mike', 'Joe', 'Willy', 'Miranda']
 
 links = [('Augustus', 'Willy'),
@@ -22,4 +30,25 @@ def crosscount(v):
 
     # Loop through every pair of links
     for i in range(len(links)):
-        for j in range(i+1, len(links)):
+        for j in range(i+1,len(links)):
+
+            # Get the locations
+            (x1,y1),(x2,y2) = loc[links[i][0]],loc[links[i][1]]
+            (x3,y3),(x4,y4) = loc[links[j][0]],loc[links[j][1]]
+
+            den=(y4-y3)*(x2-x1)-(x4-x3)*(y2-y1)
+
+            # den==0 if the lines are parallel
+            if den==0: continue
+
+            # Otherwise ua and ub are the fraction of the line where they cross
+            ua=((x4-x3)*(y1-y3)-(y4-y3)*(x1-x3))/den
+            ub=((x2-x1)*(y1-y3)-(y2-y1)*(x1-x3))/den
+
+            # If the fraction is between 0 and 1 for both lines
+            # then they cross each other
+            if ua>0 and ua<1 and ub>0 and ub<1:
+                total+=1
+    return total
+
+domain = [(10,370)] * (len(people)*2)
