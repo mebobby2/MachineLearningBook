9619cb08cc498b1e56b61509d45eb51fdb01af02
Bobby Lei <bobl53@gmail.com>
2018-01-13 17:33:55 +0800

    Enhance features

--- 6_document_filtering/feedfilter.py
+++ 6_document_filtering/feedfilter.py
@@ -25,8 +25,46 @@ def read(feed, classifier):
         fulltext='%s\n%s\n%s' % (entry['title'],entry['publisher'],entry['summary'])
 
         # Print the best guess at the current category
-        print 'Guess: '+str(classifier.classify(fulltext))
+        print 'Guess: '+str(classifier.classify(entry))
 
         # Ask the user to specify the correct category and train on that
         cl = raw_input('Enter category: ')
-        classifier.train(fulltext, cl)
+        classifier.train(entry, cl)
+
+# import feedfilter
+# import docclass
+# cl=docclass.fisherclassifier(feedfilter.entryfeatures)
+# cl.setdb('python_feed.db')
+# feedfilter.read('python_search.xml',cl)
+def entryfeatures(entry):
+    splitter = re.compile('\\W*')
+    f = {}
+
+    # Extract the title words and annotate
+    titlewords = [s.lower() for s in splitter.split(entry['title'])if len(s) > 2 and len(s) < 20]
+    for w in titlewords: f['Title:'+w] = 1
+
+    # Extract the summary words
+    summarywords = [s.lower() for s in splitter.split(entry['summary'])if len(s) > 2 and len(s) < 20]
+
+    # Count the uppercase words
+    uc = 0
+    for i in range(len(summarywords)):
+        w = summarywords[i]
+        f[w] = 1
+        if w.isupper(): uc += 1
+
+        # Get words pairs in summary as features
+        if i > len(summarywords) - 1:
+            twowords = ' '.join(summarywords[i:i+1])
+            f[twowords] = 1
+
+    # Kepp creator and publisher whole
+    f['Publisher:'+entry['publisher']] = 1
+
+    # UPPSERCASE is a virtual word flagging too much shouting
+    if float(uc)/len(summarywords) > 0.3: f['UPPERCASE'] = 1
+
+    return f
+
+
