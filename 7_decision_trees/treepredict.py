17a6b5353f6491c0dc35b4aae20deede944dd284
Bobby Lei <bobl53@gmail.com>
2018-01-19 00:42:27 +0800

    drawing the tree

--- 7_decision_trees/treepredict.py
+++ 7_decision_trees/treepredict.py
@@ -5,6 +5,8 @@
 # treepredict.entropy(set1)
 # treepredict.giniimpurity(set1)
 
+from PIL import Image,ImageDraw
+
 my_data = [['slashdot', 'USA', 'yes', 18, 'None'],
            ['google', 'France', 'yes', 23, 'Premium'],
            ['digg', 'USA', 'yes', 24, 'Basic'],
@@ -124,3 +126,63 @@ def buildtree(rows, scoref=entropy):
         return decisionnode(col = best_criteria[0], value = best_criteria[1], tb = trueBranch, fb = falseBranch)
     else:
         return decisionnode(results = uniquecounts(rows))
+
+# tree=treepredict.buildtree(treepredict.my_data)
+# treepredict.printtree(tree)
+def printtree(tree, indent = ''):
+    # Is this a leaf node?
+    if tree.results != None:
+        print str(tree.results)
+    else:
+        # Print the criteria
+        print str(tree.col) + ':' + str(tree.value) + '? '
+
+        # Print the branches
+        print indent + 'T->',
+        printtree(tree.tb, indent + '  ')
+        print indent + 'F->',
+        printtree(tree.fb, indent + '  ')
+
+def getwidth(tree):
+    if tree.tb == None and tree.fb == None: return 1
+    return getwidth(tree.tb) + getwidth(tree.fb)
+
+def getdepth(tree):
+    if tree.tb == None and tree.fb == None: return 0
+    return max(getdepth(tree.tb), getdepth(tree.fb)) + 1
+
+# tree=treepredict.buildtree(treepredict.my_data)
+# treepredict.drawtree(tree, jpeg = 'treeview.jpg')
+def drawtree(tree, jpeg = 'tree.jpg'):
+    w = getwidth(tree) * 100
+    h = getdepth(tree) * 100 + 120
+
+    img = Image.new('RGB', (w, h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+
+    drawnode(draw, tree, w/2, 20)
+    img.save(jpeg, 'JPEG')
+
+def drawnode(draw, tree, x, y):
+    if tree.results == None:
+        # Get the width of each branch
+        w1 = getwidth(tree.fb) * 100
+        w2 = getwidth(tree.tb) * 100
+
+        # Determine the total space required by this node
+        left = x - (w1 + w2)/2
+        right = x + (w1 + w2)/2
+
+        # Draw the condition string
+        draw.text((x-20, y-10),str(tree.col) + ':' + str(tree.value), (0, 0, 0))
+
+        # Draw the links to the branches
+        draw.line((x, y, left + w1 / 2, y + 100), fill=(255, 0, 0))
+        draw.line((x, y, right - w2 / 2, y + 100), fill=(255, 0, 0))
+
+        # Draw the branch nodes
+        drawnode(draw,tree.fb,left+w1/2,y+100)
+        drawnode(draw,tree.tb,right-w2/2,y+100)
+    else:
+        txt = ' \n'.join(['%s:%d' % v for v in tree.results.items()])
+        draw.text((x - 20, y), txt, (0, 0, 0))
