bb4e90f2afa514c4bd1218513e2a8fcb6c67a1b6
Bobby Lei <bobby.lei@hreasily.com>
2019-05-08 08:41:47 +1200

    Add facebook session class

--- 9_support_vector_machines/facebook.py
+++ 9_support_vector_machines/facebook.py
@@ -13,16 +13,19 @@ def getsinglevalue(node, tag):
     nl = node.getElementsByTagName(tag)
     if len(nl) > 0:
         tagNode = nl[0]
-        if tagNode.hasChildNodes()
-        return tagNode.firstChild.nodeValue
+        if tagNode.hasChildNodes():
+            return tagNode.firstChild.nodeValue
     return ''
 
 
 def callid():
     return str(int(time.time() * 10))
 
+# import facebook
+# s=facebook.fbsession()
+# friends=s.getfriends()
 
-class dbsession():
+class fbsession():
     def __init__(self):
         self.session_secret = None
         self.session_key = None
@@ -31,10 +34,48 @@ class dbsession():
         print "Press enter after logging in:", raw_input()
         self.getsession()
 
+    def getfriends(self):
+        doc = self.sendrequest({'method': 'facebook.friends.get',
+                                'session_key': self.session_key,
+                                'call_id': callid()})
+        results = []
+        for n in doc.getElementsByTagName('result_elt'):
+            results.append(n.firstChild.nodeValue)
+        return results
+
+    def getinfo(self, users):
+        ulist = ','.join(users)
+
+        fields = 'gender,current_location,relationship_status,' +\
+            'affiliations,hometown_location'
+
+        doc = self.sendrequest({'method': 'facebook.users.getInfo', 'session_key': self.session_key,
+                                'call_id': callid(), 'users': ulist, 'fields': fields})
+
+        results = {}
+        for n, id in zip(doc.getElementsByTagName('result_elt'), users):
+                # Get the location
+                locnode = n.getElementsByTagName('hometown_location')[0]
+                loc = getsinglevalue(locnode, 'city')+', ' + \
+                                    getsinglevalue(locnode, 'state')
+                # Get school
+                college = ''
+                gradyear = '0'
+                affiliations = n.getElementsByTagName('affiliations_elt')
+                for aff in affiliations:
+                    # Type 1 is college
+                    if getsinglevalue(aff, 'type') == '1':
+                        college = getsinglevalue(aff, 'name')
+                        gradyear = getsinglevalue(aff, 'year')
+                results[id] = {'gender': getsinglevalue(n, 'gender'),
+                            'status': getsinglevalue(n, 'relationship_status'),
+                            'location': loc, 'college': college, 'year': gradyear}
+        return results
+
     def sendrequest(self, args):
-        args['api_key'] = apikey
+        args['api_key'] = apiKey
         args['sig'] = self.makehash(args)
-        post_data urllib.urlencode(args)
+        post_data = urllib.urlencode(args)
         url = FacebookSecureURL + "?" + post_data
         data = urllib.urlopen(url).read()
         return parseString(data)
