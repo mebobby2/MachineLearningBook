cd07db88cbfb7da04338141b73c1fe083de1dc22
Bobby Lei <bobby.lei@hreasily.com>
2019-05-09 16:18:11 +1200

    Add factorize method

--- 10_find_independent_features/nmf.py
+++ 10_find_independent_features/nmf.py
@@ -1,12 +1,47 @@
 from numpy import *
 
-# There are many ways to measure the 'difference' between matrices
-# We use the sum of the squares of the differences between them in this case
+
 def difcost(a, b):
     dif = 0
-    # Loop over every row and column in the matrix
     for i in range(shape(a)[0]):
         for j in range(shape(a)[1]):
-          # Add together the differences
-          dif += pow(a[i,j] - b[i,j], 2)
+            # Euclidean Distance
+            dif += pow(a[i, j]-b[i, j], 2)
     return dif
+
+
+def factorize(v, pc=10, iter=50):
+    ic = shape(v)[0]
+    fc = shape(v)[1]
+
+    # Initialize the weight and feature matrices with random values
+    w = matrix([[random.random() for j in range(pc)] for i in range(ic)])
+    h = matrix([[random.random() for i in range(fc)] for i in range(pc)])
+
+    # Perform operation a maximum of iter times
+    for i in range(iter):
+        wh = w*h
+
+        # Calculate the current difference
+        cost = difcost(v, wh)
+
+        if i % 10 == 0:
+            print cost
+
+        # Terminate if the matrix has been fully factorized
+        if cost == 0:
+            break
+
+        # Update feature matrix
+        hn = (transpose(w)*v)
+        hd = (transpose(w)*w*h)
+
+        h = matrix(array(h)*array(hn)/array(hd))
+
+        # Update weights matrix
+        wn = (v*transpose(h))
+        wd = (w*h*transpose(h))
+
+        w = matrix(array(w)*array(wn)/array(wd))
+
+    return w, h
